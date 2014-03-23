6e4ea17efb8dfafc1f7fa438d2abe855c97e7fb6
Julio Viera <julio.viera@gmail.com>
2014-03-23 10:25:15 -0400

    Added test for unix time serialization #2

---
+++ src/test/java/com/github/julman99/gsonfire/gson/DateSerializationTest.java
@@ -0,0 +1,70 @@
+package com.github.julman99.gsonfire.gson;
+
+import com.github.julman99.gsonfire.DateSerializationPolicy;
+import com.github.julman99.gsonfire.GsonFireBuilder;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @autor: julio
+ */
+public class DateSerializationTest {
+
+    @Test
+    public void testUnixTimestampSeconds_serialize(){
+        Gson gson = new GsonFireBuilder()
+            .dateSerializationPolicy(DateSerializationPolicy.unixTimeSeconds)
+            .createGson();
+
+        final Date date = new Date();
+        JsonElement element = gson.toJsonTree(date);
+
+        assertEquals(date.getTime() / 1000L, element.getAsLong());
+    }
+
+    @Test
+    public void testUnixTimestampSeconds_deserialize(){
+        Gson gson = new GsonFireBuilder()
+            .dateSerializationPolicy(DateSerializationPolicy.unixTimeSeconds)
+            .createGson();
+
+        long timeNoMillis = System.currentTimeMillis() / 1000L;
+        JsonElement element = new JsonPrimitive(timeNoMillis);
+        Date parsedDate = gson.fromJson(element, Date.class);
+
+        assertEquals(timeNoMillis * 1000, parsedDate.getTime());
+    }
+
+    @Test
+    public void testUnixTimestampMillis_serialize(){
+        Gson gson = new GsonFireBuilder()
+            .dateSerializationPolicy(DateSerializationPolicy.unixTimeMillis)
+            .createGson();
+
+        final Date date = new Date();
+        JsonElement element = gson.toJsonTree(date);
+
+        assertEquals(date.getTime(), element.getAsLong());
+    }
+
+    @Test
+    public void testUnixTimestampMillis_deserialize(){
+        Gson gson = new GsonFireBuilder()
+            .dateSerializationPolicy(DateSerializationPolicy.unixTimeMillis)
+            .createGson();
+
+        long time = System.currentTimeMillis();
+        JsonElement element = new JsonPrimitive(time);
+        Date parsedDate = gson.fromJson(element, Date.class);
+
+        assertEquals(time, parsedDate.getTime());
+    }
+
+
+}
