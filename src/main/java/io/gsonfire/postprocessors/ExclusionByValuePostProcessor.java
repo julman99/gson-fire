14521088e1eb107fc320966a0d0e59932a3d79a3
João Santos <joaocpsantos@gmail.com>
2015-07-18 19:43:48 +0100

    get field naming strategy by reflection

--- src/main/java/io/gsonfire/postprocessors/ExclusionByValuePostProcessor.java
+++ src/main/java/io/gsonfire/postprocessors/ExclusionByValuePostProcessor.java
@@ -2,19 +2,23 @@ package io.gsonfire.postprocessors;
 
 import com.google.gson.*;
 import com.google.gson.annotations.SerializedName;
+import com.google.gson.internal.bind.ReflectiveTypeAdapterFactory;
 import io.gsonfire.PostProcessor;
 import io.gsonfire.annotations.ExcludeByValue;
 import io.gsonfire.gson.ExclusionByValueStrategy;
 import io.gsonfire.util.FieldInspector;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
 public final class ExclusionByValuePostProcessor implements PostProcessor {
 
     private final FieldInspector fieldInspector;
+    private FieldNamingStrategy fieldNamingStrategy;
 
     public ExclusionByValuePostProcessor(FieldInspector fieldInspector) {
         this.fieldInspector = fieldInspector;
+        this.fieldNamingStrategy = null;
     }
 
     @Override
@@ -24,7 +28,7 @@ public final class ExclusionByValuePostProcessor implements PostProcessor {
 
     @Override
     public void postSerialize(JsonElement result, Object src, Gson gson) {
-        if(src == null){
+        if(src == null || result.isJsonNull() || !result.isJsonObject()){
             return;
         }
         for(Field f: fieldInspector.getAnnotatedFields(src.getClass(), ExcludeByValue.class)){
@@ -35,7 +39,10 @@ public final class ExclusionByValuePostProcessor implements PostProcessor {
                 ExclusionByValueStrategy strategy = exclusionByValueStrategyClass.newInstance();
                 if (strategy.shouldSkipField(f.get(src))) {
                     JsonObject resultJsonObject = result.getAsJsonObject();
-                    resultJsonObject.remove(getFieldName(f));
+                    String fieldName = getFieldName(f, gson);
+                    if (fieldName != null) {
+                        resultJsonObject.remove(fieldName);
+                    }
                 } else {
                     // continue
                 }
@@ -47,8 +54,34 @@ public final class ExclusionByValuePostProcessor implements PostProcessor {
         }
     }
 
-    private String getFieldName(Field f) {
+    private String getFieldName(Field f, Gson gson) {
         SerializedName serializedName = f.getAnnotation(SerializedName.class);
-        return serializedName == null ? FieldNamingPolicy.IDENTITY.translateName(f) : serializedName.value();
+        return serializedName == null ? getFieldNamingStrategy(gson).translateName(f) : serializedName.value();
+    }
+
+    private FieldNamingStrategy getFieldNamingStrategy(Gson gson) {
+        if (this.fieldNamingStrategy == null) {
+            try {
+                Field factoriesField = gson.getClass().getDeclaredField("factories");
+                factoriesField.setAccessible(true);
+                List<TypeAdapterFactory> factories = (List<TypeAdapterFactory>) factoriesField.get(gson);
+
+                for (TypeAdapterFactory factory : factories) {
+                    if (factory instanceof ReflectiveTypeAdapterFactory) {
+                        Field fieldNamingPolicyField = factory.getClass().getDeclaredField("fieldNamingPolicy");
+                        fieldNamingPolicyField.setAccessible(true);
+                        this.fieldNamingStrategy = (FieldNamingStrategy) fieldNamingPolicyField.get(factory);
+
+                        break;
+                    }
+                }
+            } catch (NoSuchFieldException e) {
+                throw new RuntimeException(e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return this.fieldNamingStrategy;
     }
 }
