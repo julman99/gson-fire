cca375d4b07f7061dbedfb81f0d37026c0627c51
João Santos <joaocpsantos@gmail.com>
2015-07-21 19:07:30 +0100

    Add a cache per field naming strategy

--- src/main/java/io/gsonfire/util/FieldNameResolver.java
+++ src/main/java/io/gsonfire/util/FieldNameResolver.java
@@ -7,34 +7,30 @@ import com.google.gson.annotations.SerializedName;
 import com.google.gson.internal.bind.ReflectiveTypeAdapterFactory;
 
 import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.WeakHashMap;
 
 public final class FieldNameResolver {
 
-    private final Map<Field,String> cache = new ConcurrentHashMap<Field,String>();
-    private FieldNamingStrategy fieldNamingStrategy;
+    private final Map<Gson,FieldNamingStrategy> fieldNamingStrategies = new WeakHashMap<Gson, FieldNamingStrategy>();
+    private final Map<FieldNamingStrategy,Map<Field,String>> fieldNamingStrategiesFieldsCache = new WeakHashMap<FieldNamingStrategy,Map<Field,String>>();
 
     public String getFieldName(final Field field, final Gson gson) {
-        String fieldName = cache.get(field);
-        if(fieldName != null){
-            return fieldName;
-        }
+        FieldNamingStrategy fieldNamingStrategy = getFieldNamingStrategy(gson);
+        Map<Field,String> fieldNamingStrategyFieldsCache = getFieldNamingStrategyFieldsCache(fieldNamingStrategy);
+        String fieldName = fieldNamingStrategyFieldsCache.get(field);
+        if(fieldName == null){
+            SerializedName serializedName = field.getAnnotation(SerializedName.class);
+            if (serializedName == null) {
+                fieldName = getFieldNamingStrategy(gson).translateName(field);
+            } else {
+                fieldName = serializedName.value();
+            }
 
-        synchronized (cache){
-            fieldName = cache.get(field);
-            if(fieldName == null){
-                SerializedName serializedName = field.getAnnotation(SerializedName.class);
-                if (serializedName == null) {
-                    fieldName = getFieldNamingStrategy(gson).translateName(field);
-                } else {
-                    fieldName = serializedName.value();
-                }
-
-                if(!cache.containsKey(field)){
-                    cache.put(field, fieldName);
-                }
+            if(!fieldNamingStrategyFieldsCache.containsKey(field)){
+                fieldNamingStrategyFieldsCache.put(field, fieldName);
             }
         }
 
@@ -42,7 +38,9 @@ public final class FieldNameResolver {
     }
 
     private FieldNamingStrategy getFieldNamingStrategy(Gson gson) {
-        if (this.fieldNamingStrategy == null) {
+        FieldNamingStrategy fieldNamingStrategy = this.fieldNamingStrategies.get(gson);
+
+        if (fieldNamingStrategy == null) {
             try {
                 Field factoriesField = gson.getClass().getDeclaredField("factories");
                 factoriesField.setAccessible(true);
@@ -52,7 +50,9 @@ public final class FieldNameResolver {
                     if (factory instanceof ReflectiveTypeAdapterFactory) {
                         Field fieldNamingPolicyField = factory.getClass().getDeclaredField("fieldNamingPolicy");
                         fieldNamingPolicyField.setAccessible(true);
-                        this.fieldNamingStrategy = (FieldNamingStrategy) fieldNamingPolicyField.get(factory);
+
+                        fieldNamingStrategy = (FieldNamingStrategy) fieldNamingPolicyField.get(factory);
+                        this.fieldNamingStrategies.put(gson, fieldNamingStrategy);
 
                         break;
                     }
@@ -64,11 +64,28 @@ public final class FieldNameResolver {
             }
         }
 
-        if (this.fieldNamingStrategy == null) {
+        if (fieldNamingStrategy == null) {
             throw new RuntimeException("Could not get field naming strategy");
         }
 
-        return this.fieldNamingStrategy;
+        return fieldNamingStrategy;
+    }
+
+    private Map<Field,String> getFieldNamingStrategyFieldsCache(FieldNamingStrategy fieldNamingStrategy) {
+        Map<Field,String> fieldsCache = this.fieldNamingStrategiesFieldsCache.get(fieldNamingStrategy);
+        if(fieldsCache != null) {
+            return fieldsCache;
+        }
+
+        synchronized (this.fieldNamingStrategiesFieldsCache) {
+            fieldsCache = this.fieldNamingStrategiesFieldsCache.get(fieldNamingStrategy);
+            if(fieldsCache == null) {
+                fieldsCache = new HashMap<Field, String>();
+                this.fieldNamingStrategiesFieldsCache.put(fieldNamingStrategy, fieldsCache);
+            }
+        }
+
+        return fieldsCache;
     }
 
 }
